5468fad0f4f3f7df98c5ada56482358d2c51ccf3
allsyalwaa <zulfiaalsyalwa@gmail.com>
2024-10-13 21:16:22 +0700

    Add Number component

---
+++ src/components/ui/Number.jsx
@@ -0,0 +1,7 @@
+export default function Number({ number }) {
+  return (
+    <button className="bg-primary flex h-10 w-10 items-center justify-center rounded-lg text-white">
+      {number}
+    </button>
+  );
+}
