f55269f2957eafb139aa9aa42a7ede9bf9cab455
allsyalwaa <zulfiaalsyalwa@gmail.com>
2024-10-13 21:16:06 +0700

    Update tailwind.config.js to extend theme colors and container styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#002a6d",
+        secondary: "#00509c",
+      },
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "112px",
+      },
+      center: true,
+    },
   },
   plugins: [],
 };
