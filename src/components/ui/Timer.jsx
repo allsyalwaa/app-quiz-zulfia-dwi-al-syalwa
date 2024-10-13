0b7a10395c445cdec16d32bfb08c64b1ebb2a877
allsyalwaa <zulfiaalsyalwa@gmail.com>
2024-10-13 21:16:29 +0700

    Add Timer component

---
+++ src/components/ui/Timer.jsx
@@ -0,0 +1,9 @@
+export default function Timer() {
+  return (
+    <div className="flex h-20 items-center justify-end px-6">
+      <div className="rounded-lg border-[2px] px-5 py-3">
+        <p className="text-xl font-medium">1 : 30 : 45</p>
+      </div>
+    </div>
+  );
+}
